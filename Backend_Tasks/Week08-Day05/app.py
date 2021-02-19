3c5465ac7684e5efd87e06942e02e2f2f1db64bf
Nijat Gurbani <nijatgurbani@gmail.com>
2021-02-19 19:41:36 +0400

    Yenilendi

--- Backend_Tasks/Week08-Day05/app.py
+++ Backend_Tasks/Week08-Day05/app.py
@@ -1,8 +1,8 @@
 
 # # Task 1
 
-a=int(input())
-b=int(input())
+a=int(input('Ilk reqemi daxil edin : '))
+b=int(input('Ikinci reqemi daxil edin : '))
 
 print((a+b)/2)
 
@@ -19,15 +19,16 @@ def istifadeci():
 
 istifadeci()
 
+#Task 3
+
 
 
 
 #Task 4
 
-
 def soz_sayi():
 
     meqale="Baş nazir bildirib ki, müxtəlif sahələrdə əməkdaşlığımızı nəzərdə tutan bir çox sənədlər dünən keçirilən görüşlərdə imzalanıb: “Bu, çoxtərəfli platforma gələcək üçün böyük perspektivlər vəd edir. Əminəm ki, bu gün keçirilən iclasın nəticələri ikitərəfli münasibətlərin inkişafına da təkan verəcək. Prezident İlham Əliyev və Rəcəb Tayyib Ərdoğanın yorulmaz səyləri nəticəsində ölkələrimiz arasında münasibətlər ən yüksək səviyyəyə çatıb. Əməkdaşlığımız bütün sahələri əhatə edir. 2023-cü ilədək ticarət dövriyyəmizi 15 milyard dollara çatdırmaq əsas hədəflərimizdəndir”."
-    print(len(meqale))
+    print(len(meqale.split()))
     
 soz_sayi()
